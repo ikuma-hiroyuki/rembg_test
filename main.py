9a77fe52af08079787bfbd5d7f4ab0a74f5beca6
ikuma <chemicalbondo@gmail.com>
2022-11-02 11:10:03 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+from PIL import Image
+from rembg import remove
+
+if __name__ == "__main__":
+    origin_dir = Path(__file__).parent / "origin"
+    removed_dir = Path(__file__).parent / "removed"
+
+    for file in origin_dir.glob("*.jpg"):
+        print(f"processing: {file.stem}")
+        origin_image = Image.open(file)
+        removed_image = remove(origin_image)
+        removed_image.save(str(removed_dir / file.stem) + ".png")
+
+    print("Done!")
