bba55c0c1438388ab8e1230f61c05e42d6120d96
ikuma <chemicalbondo@gmail.com>
2022-11-02 11:28:08 +0900

    removedディレクトリがない場合の処理を追加

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ if __name__ == "__main__":
     origin_dir = Path(__file__).parent / "origin"
     removed_dir = Path(__file__).parent / "removed"
 
+    if not removed_dir.exists():
+        removed_dir.mkdir()
+
     for file in origin_dir.glob("*.jpg"):
         print(f"processing: {file.stem}")
         origin_image = Image.open(file)
